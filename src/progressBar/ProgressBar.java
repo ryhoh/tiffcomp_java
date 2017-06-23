80aa5fe85c0c0dc132e23394a94bec140ca71e69
堀 哲也 <axis38akasira@gmail.com>
2017-06-23 10:27:32 +0900

    ライセンス抜け修正

--- src/progressBar/ProgressBar.java
+++ src/progressBar/ProgressBar.java
@@ -1,3 +1,7 @@
+// Copyright (c) 2017 6/22 Tetsuya Hori
+// Released under the MIT license
+// https://opensource.org/licenses/mit-license.php
+
 package progressBar;
 
 public interface ProgressBar {
